224476e512b69634ef1848e91180579401f1f9c6
kmh981102 <kmh981102@naver.com>
2026-05-24 00:01:36 +0900

    feat: BE 폴더 디렉터리 구조 생성 - BE 폴더 디렉터리 구조 생성

---
+++ BE/mojak/src/main/java/com/mojak/mojak/MojakApplication.java
@@ -0,0 +1,13 @@
+package com.mojak.mojak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojakApplication.class, args);
+	}
+
+}
